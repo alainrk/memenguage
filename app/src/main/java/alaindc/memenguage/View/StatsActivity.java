188af7107904581f6796dcfc2df167a89f031dbb
alainrk <alain.narko@gmail.com>
2016-08-09 12:56:41 +0200

    Fixed settings back

--- app/src/main/java/alaindc/memenguage/View/StatsActivity.java
+++ app/src/main/java/alaindc/memenguage/View/StatsActivity.java
@@ -8,9 +8,6 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Toast;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 import alaindc.memenguage.Constants;
